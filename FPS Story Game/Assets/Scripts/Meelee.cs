f03eeca6dd3307feeeacea356c0b42bb825605a5
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2021-06-26 20:15:32 +0200

    template for weapons

---
+++ FPS Story Game/Assets/Scripts/Meelee.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Meelee : MonoBehaviour, Weapon {
+
+    public int damage;
+
+    public void Attack() {
+
+        Debug.Log("Attack Meelee " + damage);
+
+    }
+
+}

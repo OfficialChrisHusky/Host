11d642d729312f2c4f1000c08da8eb5b0c291390
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2021-06-26 20:11:21 +0200

    Weapon interface

--- FPS Story Game/Assets/Scripts/Weapon.cs
+++ FPS Story Game/Assets/Scripts/Weapon.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public interface Weapon {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Attack();
+
 }

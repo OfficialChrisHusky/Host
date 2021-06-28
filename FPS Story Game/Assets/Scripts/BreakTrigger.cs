de737b8883e67fdd0826197bef8edfae094bb464
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2021-06-28 18:24:37 +0200

    canals_1 updates

---
+++ FPS Story Game/Assets/Scripts/BreakTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BreakTrigger : MonoBehaviour {
+
+    public float Time;
+
+    private void OnTriggerEnter(Collider other) {
+
+        if(other.gameObject.tag == "Player") {
+
+            Destroy(gameObject, Time);
+
+        }
+        
+    }
+
+}

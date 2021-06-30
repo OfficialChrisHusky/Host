37f830d049de2b3128bccb684bb9812bf642697f
MfaXyz <MfaXyzOfficial@gmail.com>
2021-06-30 18:49:54 +0430

    Edit Save Script

--- FPS Story Game/Assets/Scripts/SaveSystem/Save.cs
+++ FPS Story Game/Assets/Scripts/SaveSystem/Save.cs
@@ -7,12 +7,14 @@ public class Save
     public List<Vector3Serializable> targetPositions = new List<Vector3Serializable>();
     public List<QuaternionSerializable> targetRotation = new List<QuaternionSerializable>();
 }
+[System.Serializable]
 public struct Vector3Serializable
 {
     public float x;
     public float y;
     public float z;
 }
+[System.Serializable]
 public struct QuaternionSerializable
 {
     public float x;
